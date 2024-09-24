9bb9872c87976c0385a051893822853a760d5cea
myunghun choi <myunghun0721@gmail.com>
2024-09-24 16:52:35 -0700

    server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+// test route
+app.get('/test', (_, res)=> res.send('This works'))
+
+// set port
+const port = 8080;
+const callback = () => console.log(`Server is running on port ${port}`);
+
+app.listen(port, callback)
