68f07b4f215d3a8a3d0608315f738800b1fa3895
myunghun choi <myunghun0721@gmail.com>
2024-09-26 14:22:14 -0700

    connect

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import { MONGO_URI } from "./config/keys.js";
 import { profiles } from "./routes/profiles.js";
 
 
 
 const app = express();
+app.use(express.json())
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> console.log('MongoDB is successfully connected'))
+.catch(err => console.log(err))
 
 // test route
 app.get('/test', (_, res)=> res.send('This works!'))
