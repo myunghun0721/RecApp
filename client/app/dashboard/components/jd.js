e8af3ec96c5bf485ab159e3ace18a99bce8d9721
myunghun choi <myunghun0721@gmail.com>
2024-10-16 15:04:54 -0700

    usestate

--- client/app/dashboard/components/jd.js
+++ client/app/dashboard/components/jd.js
@@ -1,5 +1,19 @@
-export const JD = () =>{
+'use client'
+
+import { useState } from 'react';
+
+
+export const JD = () => {
+    const [jobDescription, setJobDescription] = useState('');
+
+
     return (
-        <textarea placeholder="Copy Job Description Here"></textarea>
+        <>
+            <textarea
+                placeholder="Copy Job Description Here"
+                onchange={(e) => setJobDescription(e.target.value)}
+            ></textarea>
+            <button type='submit'>Submit</button>
+        </>
     )
 }
